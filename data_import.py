cfbd74c12d40b8b2dc3c6b8bc2990e3ce465c343
Nicolas Ramirez <nram.vel94@gmail.com>
2025-01-13 10:06:43 -0500

    Updated name list

--- data_import.py
+++ data_import.py
@@ -14,6 +14,7 @@ evaluadores = pd.read_csv(file_path_evaluadores)
 competencias = pd.read_excel(flie_path_competencias)
 competencia_evaluador_rol = pd.read_excel(flie_path_competencia_evaluador_rol)
 names_evaluados_list = [
+    "",
     "Luisa Fernanda Correa",
     "Cristina Duque R.",
     "Jancelly Pérez L",
@@ -40,6 +41,7 @@ names_evaluados_list = [
 ]
 
 names_evaluadores_list = [
+    "",
     "Luisa Fernanda Correa",
     "Cristina Duque R.",
     "Jancelly Pérez L",
